b5f5986cb31dc4ef8a655c0073a9bbfbe19b395c
Omar Vera <veraomar1996@gmail.com>
2025-09-12 15:08:21 -0500

    correccion de migracion de supabase

--- supabase/migrations/20250819203155_b27f4dda-cfd9-4551-9022-9e518f33de1b.sql
+++
@@ -1,6 +0,0 @@
--- Add account_id column to debt_payments table
-ALTER TABLE public.debt_payments 
-ADD COLUMN account_id uuid;
-
--- Create index for better performance
-CREATE INDEX idx_debt_payments_account_id ON public.debt_payments(account_id);
--- supabase/migrations/20250907011918_766b4712-950c-46f9-875e-591f47b30a19.sql
+++
@@ -1,8 +0,0 @@
--- Change tags column to text array to match the TypeScript interface
-ALTER TABLE public.transactions 
-ALTER COLUMN tags TYPE text[] USING 
-  CASE 
-    WHEN tags IS NULL THEN NULL
-    WHEN tags = '' THEN '{}'::text[]
-    ELSE string_to_array(tags, ',')
-  END;
